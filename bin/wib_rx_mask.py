d53146d873ca9af79c0eb744764ccf8f406b5436
Patrick Tsang <kvtsang@gmail.com>
2021-07-02 18:51:17 -0700

    Allow set rx_mask by bit

--- bin/wib_rx_mask.py
+++ bin/wib_rx_mask.py
@@ -5,22 +5,38 @@ from wib import WIB
 import wib_pb2 as wibpb
 
 import argparse
-parser = argparse.ArgumentParser(description='Python version of the wib_client utility')
+parser = argparse.ArgumentParser(
+    description='Python version of the wib_client utility'
+)
 parser.add_argument('-w', dest='wib', metavar='ip', help='wib ip address')
-parser.add_argument('--femb', type=int, choices=range(4), nargs='+', required=True,
-        help='FEMB number(s)')
+parser.add_argument(
+    '--femb', type=int, choices=range(4),
+    default=[], nargs='+', help='FEMB number(s)'
+)
+parser.add_argument(
+    'rx_mask', 
+    type=lambda x: int(x, 0),
+    default=0, nargs='?',
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     addr, __ = get_addr_port(args.wib)
 
+    if len(args.femb) > 0:
+        for i in range(4):
+            if i in args.femb: continue
+            args.rx_mask |= (0xf << (i*4))
+
+    cmd = f'mem 0xa00c0008 {hex(args.rx_mask)}'
+    print(f'[{addr}] {cmd}')
+
+
     wib = WIB(addr)
-    req = wib.defaults()
-
-    for i in args.femb:
-        req.fembs[i].enabled = True
-
+    req = wibpb.Script()
     rep = wibpb.Status()
+
+    req.script = cmd.encode()
+    req.file = False
     wib.send_command(req, rep)
-    print(rep.extra.decode('ascii'))
-    print('Successful: ',rep.success)
+    print(f'Successful: {rep.success}')
